82cd26fe5015ba74809e29b2ca6d8d8c7423bdd0
sathvik <sathvikreddy1985@gmail.com>
2023-06-29 20:02:55 +0530

    second commit

--- main.py
+++ main.py
@@ -1,28 +1,41 @@
 import hashlib
 
+
+
+b_lambda = (lambda x: hashlib.sha256(str(i).encode().hexdigest()))
+
 class Block:
     prev_block = 0
 
-    def __init__(self,nonce:int,data:str,hash:str):
+    def __init__(self,nonce:int,data:str):
         self.block = Block.prev_block + 1 
         Block.prev_block = self.block
         self.nonce = nonce
         self.data = data
-        self.prev = 0
-        self.hash = hash
+        self.prev = "0"
+        self.hash = hashlib.sha256(str(str(self.block + self.nonce ) + self.data + self.prev).encode()).hexdigest()
 
     def __repr__(self):
         return f"Block( block={self.block}, nonce={self.nonce}, data={self.data}, prev={self.prev}, hash={self.hash} )"
 
-x = Block(1,"qwerty","qwwqerwerwefwef")
-y = Block(1,"qwerty","qwwqerwerwefwef")
-z = Block(1,"qwerty","qwwqerwerwefwef")
-a = Block(1,"qwerty","qwwqerwerwefwef")
 
-print(repr(x))
-print(repr(y))
-print(repr(z))
-print(repr(a))
+block_1 = Block(1,"messi",)
+block_2 = Block(1,"ronaldo")
+block_3 = Block(1,"sterling")
+block_4 = Block(1,"cassady")
+
+
+
+blockchain = [block_1,block_2,block_3,block_4]
+
+print(repr(block_1))
+print(repr(block_2))
+print(repr(block_3))
+print(repr(block_4))
+
+
+
+
     
 
 
