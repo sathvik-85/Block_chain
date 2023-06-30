877bb14c43d5fc08b6ab0a2564511dee2e78e368
sathvik <sathvikreddy1985@gmail.com>
2023-06-30 12:45:01 +0530

    fourth commit

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import hashlib
 
 
 
-
 class Block:
     prev_block = 0
     prev_hash = "0" * 64
@@ -14,14 +13,25 @@ class Block:
         self.nonce = nonce
         self.data = data
         self.prev = Block.prev_hash 
-        self.hash = hashlib.sha256(str(str(self.block + self.nonce ) + self.data + self.prev).encode()).hexdigest()
-        Block.prev_hash = self.hash
+        Block.prev_hash = self.calculate_hash()
+
+    def calculate_hash(self):
+        return hashlib.sha256(str(str(self.block + self.nonce ) + self.data + self.prev).encode()).hexdigest()
+
 
     def __repr__(self):
-        return f"Block( block={self.block}, nonce={self.nonce}, data={self.data}, prev={self.prev}, hash={self.hash} )"
+        return f"Block( block={self.block}, nonce={self.nonce}, data={self.data}, prev={self.prev}, hash={self.calculate_hash()} )"
+    
+    def mine(self,difficulty:int):
+        while True:
+            if self.calculate_hash()[:difficulty] == "0"* difficulty:
+                return self.calculate_hash()
+            self.nonce += 1
+            continue
+            
 
 
-block_1 = Block(1,"messi",)
+block_1 = Block(1,"messiywerwerdasd",)
 block_2 = Block(1,"ronaldo")
 block_3 = Block(1,"sterling")
 block_4 = Block(1,"cassady")
@@ -30,6 +40,8 @@ block_5 = Block(1,"cassady")
 
 
 blockchain = [block_1,block_2,block_3,block_4]
+
+
 
 print(repr(block_1))
 print(repr(block_2))
@@ -40,18 +52,11 @@ print(repr(block_4))
 
 
 
-    
+print(block_1.mine(5))
 
 
 
 
-# def mine():
-#     for i in range(1000000000000):
-#         hash = hashlib.sha256(str(i).encode())
-#         hash = hash.hexdigest()
-#         print(hash)
-#         if hash[:4] == "0000":
-#             return hash
-#         continue
+
 
 # mine()
