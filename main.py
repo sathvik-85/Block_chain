1891eca61021f94d3c9296ffad23a2083c856465
sathvik <sathvikreddy1985@gmail.com>
2023-07-01 23:43:36 +0530

    latest commit

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ import hashlib
 
 class Block:
     block = 0
-    prev_global = "0" * 64
+    prev_hash = "0" * 64
 
     def __init__(self,data):
         self.block = Block.block + 1
         self.nonce = 1
         self.data = data
-        self.prev = Block.prev_global
+        self.prev = Block.prev_hash
         self.calculate_hash()
         Block.block += 1
     
@@ -17,12 +17,12 @@ class Block:
         return f"Block( block={self.block}, nonce={self.nonce}, data={self.data}, prev={self.prev[:5]}, hash={self.hash[:5]} )"
     
     def calculate_hash(self):
-        # print(self.block)
-        # print(self.nonce)
-        # print(self.data)
-        # print(self.prev)
-        # print(self.hash)
         self.hash = hashlib.sha256(str(str(self.block + self.nonce ) + self.data + self.prev).encode()).hexdigest()
+        print(self.block)
+        print(self.nonce)
+        print(self.data)
+        print(self.prev)
+        print(self.hash)
     
 
     
@@ -38,11 +38,16 @@ class BlockChain:
     def block_update(self,idx,data):
         self.blockchain[idx].data = data 
         self.blockchain[idx].calculate_hash()
+        for i in range(idx + 1, len(self.blockchain)):
+            self.blockchain[i].prev = self.blockchain[i - 1].hash
+            self.blockchain[i].calculate_hash()
+
 
 
 b1 = Block("messi")
 b2 = Block("ronaldo")
 b3 = Block("rajesh")
+b4 = Block("raja")
 
 
 
@@ -50,6 +55,7 @@ blockchain = BlockChain()
 blockchain.blockchain.append(b1)
 blockchain.blockchain.append(b2)
 blockchain.blockchain.append(b3)
+blockchain.blockchain.append(b4)
 
 
 print(repr(blockchain))                 
